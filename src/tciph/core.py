d4dec1a79ea2ec554ba3256cfece027c76f5b367
mentiferous <226847710+mentiferous@users.noreply.github.com>
2025-10-17 17:15:38 -0400

    optimize

--- src/tciph/core.py
+++ src/tciph/core.py
@@ -11,14 +11,12 @@ def gen_key():
     console.print(f"[b][*][/b] Key generated")
 
 
-def get_key():
+def key_check():
     if not Path("key.py").is_file():
         gen_key()
 
 
-def cipher():
-    get_key()
-
+def load_key():
     from key import (
         a,
         apostrophe,
@@ -97,252 +95,190 @@ def cipher():
         z,
     )
 
+    return locals()
+
+
+def cipher():
+    key_check()
+
+    key_value = load_key()
+
     text = console.input("[i]Cipher[/i]: ")
 
-    text = text.replace("a", f"{salt_0}{a}{salt_0}")
-    text = text.replace("b", f"{salt_1}{b}{salt_1}")
-    text = text.replace("c", f"{salt_2}{c}{salt_2}")
-    text = text.replace("d", d)
-    text = text.replace("e", e)
-    text = text.replace("f", f)
-    text = text.replace("g", g)
-    text = text.replace("h", h)
-    text = text.replace("i", i)
-    text = text.replace("j", j)
-    text = text.replace("k", k)
-    text = text.replace("l", l)
-    text = text.replace("m", m)
-    text = text.replace("n", n)
-    text = text.replace("o", o)
-    text = text.replace("p", p)
-    text = text.replace("q", q)
-    text = text.replace("r", r)
-    text = text.replace("s", s)
-    text = text.replace("t", t)
-    text = text.replace("u", u)
-    text = text.replace("v", v)
-    text = text.replace("w", w)
-    text = text.replace("x", x)
-    text = text.replace("y", y)
-    text = text.replace("z", z)
+    text = text.replace(
+        "a", f"{key_value['salt_0']}{key_value['a']}{key_value['salt_0']}"
+    )
+    text = text.replace(
+        "b", f"{key_value['salt_1']}{key_value['b']}{key_value['salt_1']}"
+    )
+    text = text.replace(
+        "c", f"{key_value['salt_2']}{key_value['c']}{key_value['salt_2']}"
+    )
+    text = text.replace("d", key_value["d"])
+    text = text.replace("e", key_value["e"])
+    text = text.replace("f", key_value["f"])
+    text = text.replace("g", key_value["g"])
+    text = text.replace("h", key_value["h"])
+    text = text.replace("i", key_value["i"])
+    text = text.replace("j", key_value["j"])
+    text = text.replace("k", key_value["k"])
+    text = text.replace("l", key_value["l"])
+    text = text.replace("m", key_value["m"])
+    text = text.replace("n", key_value["n"])
+    text = text.replace("o", key_value["o"])
+    text = text.replace("p", key_value["p"])
+    text = text.replace("q", key_value["q"])
+    text = text.replace("r", key_value["r"])
+    text = text.replace("s", key_value["s"])
+    text = text.replace("t", key_value["t"])
+    text = text.replace("u", key_value["u"])
+    text = text.replace("v", key_value["v"])
+    text = text.replace("w", key_value["w"])
+    text = text.replace("x", key_value["x"])
+    text = text.replace("y", key_value["y"])
+    text = text.replace("z", key_value["z"])
 
-    text = text.replace("A", capital_a)
-    text = text.replace("B", capital_b)
-    text = text.replace("C", capital_c)
-    text = text.replace("D", capital_d)
-    text = text.replace("E", capital_e)
-    text = text.replace("F", capital_f)
-    text = text.replace("G", capital_g)
-    text = text.replace("H", capital_h)
-    text = text.replace("I", capital_i)
-    text = text.replace("J", capital_j)
-    text = text.replace("K", capital_k)
-    text = text.replace("L", capital_l)
-    text = text.replace("M", capital_m)
-    text = text.replace("N", capital_n)
-    text = text.replace("O", capital_o)
-    text = text.replace("P", capital_p)
-    text = text.replace("Q", capital_q)
-    text = text.replace("R", capital_r)
-    text = text.replace("S", capital_s)
-    text = text.replace("T", capital_t)
-    text = text.replace("U", capital_u)
-    text = text.replace("V", capital_v)
-    text = text.replace("W", capital_w)
-    text = text.replace("X", capital_x)
-    text = text.replace("Y", capital_y)
-    text = text.replace("Z", capital_z)
+    text = text.replace("A", key_value["capital_a"])
+    text = text.replace("B", key_value["capital_b"])
+    text = text.replace("C", key_value["capital_c"])
+    text = text.replace("D", key_value["capital_d"])
+    text = text.replace("E", key_value["capital_e"])
+    text = text.replace("F", key_value["capital_f"])
+    text = text.replace("G", key_value["capital_g"])
+    text = text.replace("H", key_value["capital_h"])
+    text = text.replace("I", key_value["capital_i"])
+    text = text.replace("J", key_value["capital_j"])
+    text = text.replace("K", key_value["capital_k"])
+    text = text.replace("L", key_value["capital_l"])
+    text = text.replace("M", key_value["capital_m"])
+    text = text.replace("N", key_value["capital_n"])
+    text = text.replace("O", key_value["capital_o"])
+    text = text.replace("P", key_value["capital_p"])
+    text = text.replace("Q", key_value["capital_q"])
+    text = text.replace("R", key_value["capital_r"])
+    text = text.replace("S", key_value["capital_s"])
+    text = text.replace("T", key_value["capital_t"])
+    text = text.replace("U", key_value["capital_u"])
+    text = text.replace("V", key_value["capital_v"])
+    text = text.replace("W", key_value["capital_w"])
+    text = text.replace("X", key_value["capital_x"])
+    text = text.replace("Y", key_value["capital_y"])
+    text = text.replace("Z", key_value["capital_z"])
 
-    text = text.replace("0", digit_0)
-    text = text.replace("1", digit_1)
-    text = text.replace("2", digit_2)
-    text = text.replace("3", digit_3)
-    text = text.replace("4", digit_4)
-    text = text.replace("5", digit_5)
-    text = text.replace("6", digit_6)
-    text = text.replace("7", digit_7)
-    text = text.replace("8", digit_8)
-    text = text.replace("9", digit_9)
+    text = text.replace("0", key_value["digit_0"])
+    text = text.replace("1", key_value["digit_1"])
+    text = text.replace("2", key_value["digit_2"])
+    text = text.replace("3", key_value["digit_3"])
+    text = text.replace("4", key_value["digit_4"])
+    text = text.replace("5", key_value["digit_5"])
+    text = text.replace("6", key_value["digit_6"])
+    text = text.replace("7", key_value["digit_7"])
+    text = text.replace("8", key_value["digit_8"])
+    text = text.replace("9", key_value["digit_9"])
 
-    text = text.replace(" ", space)
-    text = text.replace(".", period)
-    text = text.replace(",", comma)
-    text = text.replace("'", apostrophe)
-    text = text.replace("?", question_mark)
-    text = text.replace("!", exclamation_mark)
-    text = text.replace("-", hyphen)
-    text = text.replace("=", equals)
-    text = text.replace("_", underscore)
+    text = text.replace(" ", key_value["space"])
+    text = text.replace(".", key_value["period"])
+    text = text.replace(",", key_value["comma"])
+    text = text.replace("'", key_value["apostrophe"])
+    text = text.replace("?", key_value["question_mark"])
+    text = text.replace("!", key_value["exclamation_mark"])
+    text = text.replace("-", key_value["hyphen"])
+    text = text.replace("=", key_value["equals"])
+    text = text.replace("_", key_value["underscore"])
 
-    if rotate == 1:
+    if key_value["rotate"] == 1:
         text = text[::-1]
 
     console.print(f"[i]Ciphered[/i]: {text}")
 
 
 def decipher():
-    get_key()
+    key_check()
 
-    from key import (
-        a,
-        apostrophe,
-        b,
-        c,
-        capital_a,
-        capital_b,
-        capital_c,
-        capital_d,
-        capital_e,
-        capital_f,
-        capital_g,
-        capital_h,
-        capital_i,
-        capital_j,
-        capital_k,
-        capital_l,
-        capital_m,
-        capital_n,
-        capital_o,
-        capital_p,
-        capital_q,
-        capital_r,
-        capital_s,
-        capital_t,
-        capital_u,
-        capital_v,
-        capital_w,
-        capital_x,
-        capital_y,
-        capital_z,
-        comma,
-        d,
-        digit_0,
-        digit_1,
-        digit_2,
-        digit_3,
-        digit_4,
-        digit_5,
-        digit_6,
-        digit_7,
-        digit_8,
-        digit_9,
-        e,
-        equals,
-        exclamation_mark,
-        f,
-        g,
-        h,
-        hyphen,
-        i,
-        j,
-        k,
-        l,
-        m,
-        n,
-        o,
-        p,
-        period,
-        q,
-        question_mark,
-        r,
-        rotate,
-        s,
-        salt_0,
-        salt_1,
-        salt_2,
-        space,
-        t,
-        u,
-        underscore,
-        v,
-        w,
-        x,
-        y,
-        z,
-    )
+    key_value = load_key()
 
     text = console.input("[i]Decipher[/i]: ")
 
-    if rotate == 1:
+    if key_value["rotate"] == 1:
         text = text[::-1]
 
-    text = text.replace(salt_0, "")
-    text = text.replace(salt_1, "")
-    text = text.replace(salt_2, "")
+    text = text.replace(key_value["salt_0"], "")
+    text = text.replace(key_value["salt_1"], "")
+    text = text.replace(key_value["salt_2"], "")
 
-    text = text.replace(a, "a")
-    text = text.replace(b, "b")
-    text = text.replace(c, "c")
-    text = text.replace(d, "d")
-    text = text.replace(e, "e")
-    text = text.replace(f, "f")
-    text = text.replace(g, "g")
-    text = text.replace(h, "h")
-    text = text.replace(i, "i")
-    text = text.replace(j, "j")
-    text = text.replace(k, "k")
-    text = text.replace(l, "l")
-    text = text.replace(m, "m")
-    text = text.replace(n, "n")
-    text = text.replace(o, "o")
-    text = text.replace(p, "p")
-    text = text.replace(q, "q")
-    text = text.replace(r, "r")
-    text = text.replace(s, "s")
-    text = text.replace(t, "t")
-    text = text.replace(u, "u")
-    text = text.replace(v, "v")
-    text = text.replace(w, "w")
-    text = text.replace(x, "x")
-    text = text.replace(y, "y")
-    text = text.replace(z, "z")
+    text = text.replace(key_value["a"], "a")
+    text = text.replace(key_value["b"], "b")
+    text = text.replace(key_value["c"], "c")
+    text = text.replace(key_value["d"], "d")
+    text = text.replace(key_value["e"], "e")
+    text = text.replace(key_value["f"], "f")
+    text = text.replace(key_value["g"], "g")
+    text = text.replace(key_value["h"], "h")
+    text = text.replace(key_value["i"], "i")
+    text = text.replace(key_value["j"], "j")
+    text = text.replace(key_value["k"], "k")
+    text = text.replace(key_value["l"], "l")
+    text = text.replace(key_value["m"], "m")
+    text = text.replace(key_value["n"], "n")
+    text = text.replace(key_value["o"], "o")
+    text = text.replace(key_value["p"], "p")
+    text = text.replace(key_value["q"], "q")
+    text = text.replace(key_value["r"], "r")
+    text = text.replace(key_value["s"], "s")
+    text = text.replace(key_value["t"], "t")
+    text = text.replace(key_value["u"], "u")
+    text = text.replace(key_value["v"], "v")
+    text = text.replace(key_value["w"], "w")
+    text = text.replace(key_value["x"], "x")
+    text = text.replace(key_value["y"], "y")
+    text = text.replace(key_value["z"], "z")
 
-    text = text.replace(capital_a, "A")
-    text = text.replace(capital_b, "B")
-    text = text.replace(capital_c, "C")
-    text = text.replace(capital_d, "D")
-    text = text.replace(capital_e, "E")
-    text = text.replace(capital_f, "F")
-    text = text.replace(capital_g, "G")
-    text = text.replace(capital_h, "H")
-    text = text.replace(capital_i, "I")
-    text = text.replace(capital_j, "J")
-    text = text.replace(capital_k, "K")
-    text = text.replace(capital_l, "L")
-    text = text.replace(capital_m, "M")
-    text = text.replace(capital_n, "N")
-    text = text.replace(capital_o, "O")
-    text = text.replace(capital_p, "P")
-    text = text.replace(capital_q, "Q")
-    text = text.replace(capital_r, "R")
-    text = text.replace(capital_s, "S")
-    text = text.replace(capital_t, "T")
-    text = text.replace(capital_u, "U")
-    text = text.replace(capital_v, "V")
-    text = text.replace(capital_w, "W")
-    text = text.replace(capital_x, "X")
-    text = text.replace(capital_y, "Y")
-    text = text.replace(capital_z, "Z")
+    text = text.replace(key_value["capital_a"], "A")
+    text = text.replace(key_value["capital_b"], "B")
+    text = text.replace(key_value["capital_c"], "C")
+    text = text.replace(key_value["capital_d"], "D")
+    text = text.replace(key_value["capital_e"], "E")
+    text = text.replace(key_value["capital_f"], "F")
+    text = text.replace(key_value["capital_g"], "G")
+    text = text.replace(key_value["capital_h"], "H")
+    text = text.replace(key_value["capital_i"], "I")
+    text = text.replace(key_value["capital_j"], "J")
+    text = text.replace(key_value["capital_k"], "K")
+    text = text.replace(key_value["capital_l"], "L")
+    text = text.replace(key_value["capital_m"], "M")
+    text = text.replace(key_value["capital_n"], "N")
+    text = text.replace(key_value["capital_o"], "O")
+    text = text.replace(key_value["capital_p"], "P")
+    text = text.replace(key_value["capital_q"], "Q")
+    text = text.replace(key_value["capital_r"], "R")
+    text = text.replace(key_value["capital_s"], "S")
+    text = text.replace(key_value["capital_t"], "T")
+    text = text.replace(key_value["capital_u"], "U")
+    text = text.replace(key_value["capital_v"], "V")
+    text = text.replace(key_value["capital_w"], "W")
+    text = text.replace(key_value["capital_x"], "X")
+    text = text.replace(key_value["capital_y"], "Y")
+    text = text.replace(key_value["capital_z"], "Z")
 
-    text = text.replace(digit_0, "0")
-    text = text.replace(digit_1, "1")
-    text = text.replace(digit_2, "2")
-    text = text.replace(digit_3, "3")
-    text = text.replace(digit_4, "4")
-    text = text.replace(digit_5, "5")
-    text = text.replace(digit_6, "6")
-    text = text.replace(digit_7, "7")
-    text = text.replace(digit_8, "8")
-    text = text.replace(digit_9, "9")
+    text = text.replace(key_value["digit_0"], "0")
+    text = text.replace(key_value["digit_1"], "1")
+    text = text.replace(key_value["digit_2"], "2")
+    text = text.replace(key_value["digit_3"], "3")
+    text = text.replace(key_value["digit_4"], "4")
+    text = text.replace(key_value["digit_5"], "5")
+    text = text.replace(key_value["digit_6"], "6")
+    text = text.replace(key_value["digit_7"], "7")
+    text = text.replace(key_value["digit_8"], "8")
+    text = text.replace(key_value["digit_9"], "9")
 
-    text = text.replace(space, " ")
-    text = text.replace(period, ".")
-    text = text.replace(comma, ",")
-    text = text.replace(apostrophe, "'")
-    text = text.replace(question_mark, "?")
-    text = text.replace(exclamation_mark, "!")
-    text = text.replace(hyphen, "-")
-    text = text.replace(equals, "=")
-    text = text.replace(underscore, "_")
+    text = text.replace(key_value["space"], " ")
+    text = text.replace(key_value["period"], ".")
+    text = text.replace(key_value["comma"], ",")
+    text = text.replace(key_value["apostrophe"], "'")
+    text = text.replace(key_value["question_mark"], "?")
+    text = text.replace(key_value["exclamation_mark"], "!")
+    text = text.replace(key_value["hyphen"], "-")
+    text = text.replace(key_value["equals"], "=")
+    text = text.replace(key_value["underscore"], "_")
 
     console.print(f"[i]Deciphered[/i]: {text}")
